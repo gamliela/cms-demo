d36bfe81953a2db97c0f001579ca91d7834814b7
Alon Gamliel <alon@gamliela.com>
2017-12-07 15:42:55 +0200

    Forking my own repo starter-react-mobx-css-modules

---
+++ README.md
@@ -0,0 +1,22 @@
+# starter-react-mobx-css-modules
+My starter kit for projects based on React, MobX, CSS Modules and TypeScript.
+
+This kit consists of the latest versions of:
+* React
+* Webpack
+* MobX
+* SCSS
+* CSS Modules
+* TypeScript
+
+#### Build Instructions
+
+`npm install`
+
+`npm build`
+
+The output goes to `/build/bundle.js` & `/build/index.html`
+
+#### Development Server
+
+`npm dev-server`
---
+++ src/app/pages/MainPage/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import {observer} from "mobx-react";
+import style from "./style.scss";
+import {AppManager} from "../../AppManager";
+
+export interface MainPageProps {
+    appManager: AppManager;
+}
+
+@observer
+export default class MainPage extends React.Component<MainPageProps> {
+    render() {
+        const appManager = this.props.appManager;
+        return (
+            <div>
+                <h1 className={style.header}>
+                    Hello World!
+                </h1>
+                <span>version: {appManager.version}</span>
+            </div>
+        );
+    }
+}
